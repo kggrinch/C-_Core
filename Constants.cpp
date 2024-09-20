4d7dc92d65fc0c8eb3f92a9cb0fa5b273a4a18f6
Kirill <kirillkongrichanichenko@gmail.com>
2024-09-20 13:58:55 -0700

    Updating core from windows

--- Constants.cpp
+++ Constants.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+// Example of const in functions of classes
+class Family
+{
+  private:
+    int size = 5;
+  public:
+    int sizeOfFamily() const // This const keyword makes sure that the function in this class does not change any variables that is in the class
+      {
+          // size = 10; // This wont work
+          return size;
+      }
+};
+
+// Declared function
+void printInfo(const std::string &name, const int &age);
+
+
 int main()
 {
-  // const keyword - specifices that a variable's value is constant.
+  // const keyword - specifics that a variable's value is constant.
   //                 Tells the compiler to prevent anything from modifying it.
   //                 (read-only)
 
@@ -14,7 +31,30 @@ int main()
   double circumference = 2 * PI * RADIUS;
 
   // Printing circumference
-  std::cout << circumference << "cm"; 
+  std::cout << circumference << "cm" << '\n'; 
+
+  // constant function - A constant function can only be made inside a class, and it is used for when you want to do something in a function but you dont want to allow that function
+  //                     to modify the classes attributes. Used for stuff like getSize() method.
+
+  // Initializing and printing family size
+  Family family;
+  std::cout << family.sizeOfFamily() << '\n';
+
+  // Constant parameter - a parameter that is effectively read-only. Code is more secure and conveys intent. Useful for references and pointers
+  printInfo("Bob", 21);
+
+
+
+
 
   return 0;
 }
+
+void printInfo(const std::string &name, const int &age)
+{
+    // name = "Not Bob"; // These are not allowed since name and age are const and cannot be modified
+    // age = 0; 
+
+    std::cout << name << '\n';
+    std::cout << age << '\n';
+};
